74657494c7f080002112747d14e89657c26567bc
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2022-01-01 02:18:15 +0300

    cmd/gogrep: implement match vars interpolation in --format (#15)

--- cmd/gogrep/match.go
+++ cmd/gogrep/match.go
@@ -1,12 +1,24 @@
 package main
 
+import (
+	"github.com/quasilyte/gogrep"
+)
+
 type match struct {
 	text             string
 	matchStartOffset int
 	matchLength      int
 
-	filename string
-	line     int
-	startPos int
-	endPos   int
+	capture []capturedNode
+
+	filename    string
+	line        int
+	startOffset int
+	endOffset   int
+}
+
+type capturedNode struct {
+	startOffset int
+	endOffset   int
+	data        gogrep.CapturedNode
 }
